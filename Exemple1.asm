a64defa205d4f8d2fbac4662cd37e3438465a7e5
DaHappyNoobie <21h.cuzon@gmail.com>
2016-12-14 15:32:04 +0100

    Add files via upload
    Premier programme d'exemple en assembleur

---
+++ Exemple1.asm
@@ -0,0 +1,21 @@
+
+.data
+
+	var1: .byte 42		#var1 est un octet en mémoire de valeur 42
+	var2: .byte 4		#var2 est un octet en mémoire de valeur 4
+	result: .space 1	#result est un octet vide en mémoire
+
+.text
+
+main:
+	lw $t0, var1		#on charge var1 dans t0
+	lw $t1, var2		#on charge var2 dans t1
+	li $t3, 5		#on charge 5 dans t3
+	div $t0, $t1		#on divise t0 par t1
+	mfhi $t4		#on charge le reste de la division dans t4
+	slt $t0, $t4, $t3	#on met t0 à 1 si t4 < t3
+	sll $t0, $t0, 2		#on décale de 2 bits à gauche la valeur de t0
+	sw $t0, result		#on stocke t0 dans la mémoire à result
+
+	li $v0, 10		#on sélectionne le syscall de sortie
+	syscall			#on appelle le syscall (sortie)
