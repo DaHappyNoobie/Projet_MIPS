12ca550278d8760a539def606c3c34450f690e1b
thalfeust <thalfeust@gmail.com>
2017-01-02 11:04:36 +0100

    Add files via upload

---
+++ fichier.c
@@ -0,0 +1,76 @@
+#include "fichier.h"
+
+void consoleLectureCommande() {
+	
+	viderBuffer();
+
+	printf("\nEntrer la ligne de commande : ");
+	/*scanf("%[^\n]",&commande);*/
+	fgets(commandeString, 30, stdin);
+}
+
+void fichierLectureCommande(FILE * fichier) {
+
+	for(int i=0; i<30;i++){
+		commandeString[i] = ' ';
+	}
+
+	if (fgets(commandeString,30,fichier)!=NULL) {
+		
+		puts(commandeString);
+	}
+}
+
+char ecritUCharTab(FILE * fichier, char type) {
+
+	if (type == 'b') {	
+
+		for (int i=0;i<32;i++) {
+		
+		fprintf(fichier,"%c", commandeBinaire[i]);
+	
+		}
+	
+	}
+	else if (type == 'h') {
+	
+		for (int i=0;i<8;i++) {
+		
+		fprintf(fichier,"%c", commandeHexa[i]);
+	
+		}
+	
+	}
+	else printf("\nMauvais type.\n");
+	return '1';
+}
+
+char ecritListe(FILE * fichierB, FILE * fichierH, listeCommande *l) {
+
+	commande *courant;
+	courant = *l;
+
+	while ( courant != NULL ) {
+
+		for (int i=0;i<32;i++) {
+			fprintf(fichierB,"%c", courant->codeBinaire[i]);
+		}
+
+		for (int i=0;i<8;i++) {		
+			fprintf(fichierH,"%c", courant->codeHexa[i]);	
+		}
+		
+		courant = courant->suivant;
+	}
+
+	return '1';
+}
+
+void viderBuffer() { /* Cette fonction nous permet d'etre sur que le buffer est vide. */
+   
+   	char c;
+
+    do { 
+        c = getchar(); 
+    } while (c != '\n' && c != EOF); 
+}
